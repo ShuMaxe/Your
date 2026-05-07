8f623eaf9eb9971532e4d5533df451dda8e930ea
ShuMaxe <prokunin.sergey@mail.ru>
2026-05-07 16:43:16 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
+// config.js
 window.BOT_TOKEN = '8535857784:AAHog3Jdp4XADsfDR_HZGOUtXtqrpw9jCh4';
 window.CHAT_ID = '558550427';
 
 console.log("config.js загружен, BOT_TOKEN =", window.BOT_TOKEN);
+console.log("config.js загружен, CHAT_ID =", window.CHAT_ID);
