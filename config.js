48246cd012cf6def331704847247b512613d2e6d
ShuMaxe <prokunin.sergey@mail.ru>
2026-05-07 16:35:32 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,4 @@
-const BOT_TOKEN = '8535857784:AAHog3Jdp4XADsfDR_HZGOUtXtqrpw9jCh4;
-const CHAT_ID = '558550427';
+window.BOT_TOKEN = '8535857784:AAHog3Jdp4XADsfDR_HZGOUtXtqrpw9jCh4';
+window.CHAT_ID = '558550427';
+
+console.log("config.js загружен, BOT_TOKEN =", window.BOT_TOKEN);
